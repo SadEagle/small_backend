2042207028e80c9137bec524dc8430da97c81892
Sandy Solomon <sadsandeagle@gmail.com>
2025-09-07 19:25:21 +0500

    Add alembic constraint naming convention
    Alembic namig convention needed otherwise it's impossible to track those
    constraints

--- backend/app/model_db.py
+++ backend/app/model_db.py
@@ -1,11 +1,21 @@
 from datetime import datetime
 
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey, func
+from sqlalchemy import MetaData, ForeignKey, func
 
 
+# Alembic naming constraints convention
+# Ref: https://alembic.sqlalchemy.org/en/latest/naming.html#integration-of-naming-conventions-into-operations-autogenerate
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
 
 
 class QuestionDB(Base):
